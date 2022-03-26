92b581999086b40533f4ee314d4df2d810b2d937
nguyennhuvinh <2051063937@e.tlu.edu.vn>
2022-03-26 20:25:22 +0700

    khởi tạo trang admin vs hoàn thành trang admin-login

---
+++ Assets/js/admin.js
@@ -0,0 +1,10 @@
+document.getElementById("login").disabled=true;
+
+function check(){
+    var u = document.getElementById("username").value;
+    var p = document.getElementById("password").value;
+    if (u =="admin" && p =="123") 
+        document.getElementById("login").disabled=false;
+    else
+        document.getElementById("login").disabled=true;
+}
